923c9f8b9e70838d418b78bdb4498953f65cd3df
JuanD <dosquisd123@gmail.com>
2026-02-04 21:54:30 -0500

    feat: add setup_plotting function for improved graph styling with scienceplots

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,26 @@
+import matplotlib.pyplot as plt
+import scienceplots  # noqa: F401
+
 from utils.constants import DATADIR, NAN_THRESHOLD, ROOTDIR
 from utils.dtypes import AdjacencyMethod, EventData, Graph
 from utils.graph import GraphEvent
 from utils.load import load_data, load_events
 from utils.normalize import Normalizer
+
+
+# LaTeX must be installed previously for this to work
+def setup_plotting():
+    plt.style.use(["science", "nature"])
+    plt.rcParams.update(
+        {
+            "font.size": 12,
+            "xtick.labelsize": 12,
+            "ytick.labelsize": 12,
+            "axes.labelsize": 12,
+            "legend.fontsize": 12,
+        }
+    )
+
 
 __all__ = [
     "DATADIR",
@@ -15,4 +33,5 @@ __all__ = [
     "load_data",
     "load_events",
     "Normalizer",
+    "setup_plotting",
 ]
